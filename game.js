a6b771b49135d54d3088b34dac7be12475abddf2
JJ-3233 <jabersonlee@gmail.com>
2025-12-27 18:39:55 +0800

    Update game.js

--- game.js
+++ game.js
@@ -81,7 +81,7 @@ function updateStats() {
 function updateDifficultyAndUI() {
   // 🔧 測試方便：每 100 分升一級
   // 之後你要正式版，把 100 改回 1000 即可
-  if(level<5)｛
+  if(minGroupToClear<10)｛
     const newLevel = 1 + Math.floor(score / 1000);
     level = newLevel;
   ｝
